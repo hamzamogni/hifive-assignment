52fcbbdba57737bfb5968170a2e81f0e73c241b9
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 23:23:08 +0100

    implement mai axios instance

---
+++ webapp/src/api/Api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default() => {
+    let baseURL = "http://localhost:8000/api";
+    
+    return axios.create({
+        baseURL: baseURL,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+}
