b4693a6eac127e408cb1b93a7a11deaf75b7a4f4
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 19:01:27 +0100

    implement product repository

--- server/app/Providers/RepositoryServiceProvider.php
+++ server/app/Providers/RepositoryServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Repositories\Eloquent\BaseRepository;
 use App\Repositories\Eloquent\CategoryRepository;
+use App\Repositories\Eloquent\ProductRepository;
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
 use App\Repositories\Interfaces\EloquentRepositoryInterface;
+use App\Repositories\Interfaces\ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -20,6 +22,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
 
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
     }
 
     /**
