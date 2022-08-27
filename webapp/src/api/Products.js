48c9db29ecb86a19e2f32e227f243f60b15b7c31
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 23:23:26 +0100

    implement get and create actions in Product Api layer

---
+++ webapp/src/api/Products.js
@@ -0,0 +1,20 @@
+import Api from "./Api";
+
+export default {
+    async getProducts() {
+        return await Api().get("/products");
+    },
+
+    async getProduct(id) {
+        return await Api().get(`/products/${id}`);
+    },
+
+    async createProduct(data) {
+        return await Api().post("/products", {
+            "name": data.name,
+            "price": data.price,
+            "description": data.description,
+        })
+    }
+
+}
