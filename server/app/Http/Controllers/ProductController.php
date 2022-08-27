42debb20634cce84be73896e0f0a7e628433cc33
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 19:09:34 +0100

    implement show route for products

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -50,7 +50,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return ProductResource::make($product);
     }
 
     /**
