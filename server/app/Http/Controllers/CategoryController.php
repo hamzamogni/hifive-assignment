4715c305397872f11de5c1f4a9a8f04712b626c1
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 18:32:56 +0100

    implement show route for categories

--- server/app/Http/Controllers/CategoryController.php
+++ server/app/Http/Controllers/CategoryController.php
@@ -49,7 +49,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return CategoryResource::make($category);
     }
 
     /**
