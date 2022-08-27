fdcc9fb15181c92f0eff148bcf603b2525e70d6f
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-27 23:22:56 +0100

    delete boilerplate

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -17,20 +17,17 @@
         text
       >
         <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
 };
 </script>
