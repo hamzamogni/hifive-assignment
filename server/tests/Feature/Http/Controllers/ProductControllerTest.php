1747ff3bc85859f76344546eddb5f4aad8a99dec
Hamza Mogni <hamzamogni5@gmail.com>
2022-08-28 19:33:27 +0100

    implement feature test for product update

--- server/tests/Feature/Http/Controllers/ProductControllerTest.php
+++ server/tests/Feature/Http/Controllers/ProductControllerTest.php
@@ -2,21 +2,58 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+    
+    public function testIndexProductsReturnsProducts()
     {
-        $response = $this->get('/');
+        Product::factory()->count(10)->create();
 
-        $response->assertStatus(200);
+        $response = $this->getJson('/api/products');
+
+        $response->assertJson(fn (AssertableJson $json) => 
+            $json->has("data", 10, fn ($json) => 
+                $json->hasAll(["id", "name", "description", "price", "categories"])
+            )
+        );
     }
+
+    public function testCreateProductPersistsProduct()
+    {
+        $data = [
+            "name" => "test",
+            "description" => "test description", 
+            "price" => 120.9
+        ];
+
+        $this->postJson("/api/products", $data);
+        $this->assertDatabaseHas("products", $data);
+    }
+
+    public function testUpdateProductRouteUpdatesProduct()
+    {
+        $product = Product::create([
+            "name" => "test",
+            "description" => "test description",
+            "price" => 120,
+        ]);
+
+        $this->patchJson("/api/products/{$product->id}", [
+            "name" => "new name"
+        ]);
+
+        $this->assertDatabaseHas("products", [
+            "id" => $product->id,
+            "name" => "new name"
+        ]);
+    }
+
+
 }
